bda96684aeafb38d892f9deef7ce6f12acca5e79
Kinderman de Freitas Barros <72263909+KindermanBarros@users.noreply.github.com>
2026-09-06 11:45:03 -0400

    feat: add admin app screens and navigation

---
+++ app/src/main/java/com/eloverde/admin/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.eloverde.admin
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import com.eloverde.admin.presentation.AdminApp
+
+class MainActivity : ComponentActivity() { override fun onCreate(savedInstanceState: Bundle?) { super.onCreate(savedInstanceState); setContent { MaterialTheme { AdminApp() } } } }
